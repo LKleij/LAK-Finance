a146c8d6df3ecba0998020ebd8f1e506f6d2b106
Lucas <kleijvander@gmail.com>
2021-12-09 11:06:02 +0100

    fixes error message

--- src/app/pages/auth/store/auth.reducer.ts
+++ src/app/pages/auth/store/auth.reducer.ts
@@ -57,10 +57,12 @@ function handleError(errorMessage: string) {
         case 'TOO_MANY_ATTEMPTS_TRY_LATER':
             return 'We have blocked all requests from this device due to unusual activity. Try again later.'
         case 'EMAIL_NOT_FOUND':
-            return 'The email address is already in use by another account.'
+            return 'There is no user record corresponding to this identifier. The user may have been deleted.'
         case 'INVALID_PASSWORD':
             return 'The password is invalid or the user does not have a password.'
         case 'USER_DISABLED':
             return 'The user account has been disabled by an administrator.'
+        default:
+            return 'Something went wrong.'
     }
 }
